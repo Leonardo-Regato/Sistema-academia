c1371544d2b55c4e30841f6a193610a913d96e58
Leonardo Regato <leonardoregat@gmail.com>
2026-09-24 22:36:53 -0300

    feat: implementação CRUD e validações

--- __main__.py
+++ __main__.py
@@ -1,32 +1,226 @@
 from classes import *
-from datetime import datetime, date
-
+from rich import print
+from rich.panel import Panel
+from time import sleep
 
 
 def main():
-
-    a1 = Aluno("Leonardo", "22/08/2006")
-    a2 = Aluno("Julia", "18/03/2007")
-    a3 = Aluno("Luiz", "10/08/2006")
-
-    p1 = Professor("Leandro", "25/04/1979")
-    p2 = Professor("Patrcia", "26/08/1978")
-
     academia = Academia()
+    conteudo = (
+        "\n[yellow]1[/]  - [green3]Cadastrar aluno[/]\n"
+        "[yellow]2[/]  - [green3]Buscar alunos[/]\n"
+        "[yellow]3[/]  - [green3]Alterar aluno[/]\n"
+        "[yellow]4[/]  - [green3]Remover aluno[/]\n"
+        "[yellow]5[/]  - [green3]Listar alunos[/]\n\n"
 
-    academia.add_aluno(a1)
-    academia.add_aluno(a2)
-    academia.add_aluno(a3)
+        "[yellow]6[/]  - [green3]Cadastrar professor[/]\n"
+        "[yellow]7[/]  - [green3]Buscar professor[/]\n"
+        "[yellow]8[/]  - [green3]Alterar professor[/]\n"
+        "[yellow]9[/]  - [green3]Remover professor[/]\n"
+        "[yellow]10[/] - [green3]Listar professores[/]\n\n"
+        "[yellow]0[/]  - [red]Sair[/]"
 
-    academia.add_professor(p1)
-    academia.add_professor(p2)
+    )
+    while True:
+        painel = Panel(conteudo, title=" Sistema academia ", width=40)
+        print(painel)
+        try:
+            resp = int(input("Escolha uma opção: "))
+        except ValueError:
+            print("[red]ERRO:[/] Digite apenas um número.")
+            sleep(1)
+            continue
+        if not 0 <= resp <= 10:
+            print("[red]ERRO:[/]  Opção INVÁLIDA")
+            sleep(1)
+            continue
+        
+        
 
-    academia.mostrar_alunos()
-    academia.mostrar_professores()
+        match resp:
+            case 0:
+                break
+            case 1:
+                while True:
+                    try:
+                        nome_aluno = input("Digite o nome do aluno: ")
+                        Pessoa.validar_nome(nome_aluno)
+                        break
+                    except ValueError as e:
+                        print(f"[red]ERRO:[/]  {e}")
+                while True:
+                    try:
+                        data = input("Digite a data de nascimento. ex(00/00/0000): ")
+                        academia.cadastra_aluno(nome_aluno, data)
+                        print("Aluno cadastrado com [green]SUCESSO[/].")
+                        sleep(1)
+                        break
+                    except ValueError as e:
+                        print(f"[red]ERRO:[/]  {e}")
 
-    academia.buscar_aluno("Leonardo")
-    academia.buscar_professor("Luiz")
 
+
+            case 2:
+                nome_busca = input("Digite o nome do aluno: ")
+                academia.buscar_aluno(nome_busca)
+                sleep(1)
+
+            case 3:
+                if not academia.lista_alunos:
+                    print("Ainda não temos alunos cadastrados... Voltando ao menu")
+                    sleep(1)
+                    continue
+                id_aluno = academia.validar_id(academia.localizar_aluno)
+                    
+                while True:
+                    alterar = input("Alterar nome ou data? [digite apenas um]: ")
+                    if alterar.lower().strip() == "nome":
+                        while True:
+                            try:
+                                novo_nome = input("Digite o novo nome: ")
+                                academia.alterar_nome_aluno(id_aluno, novo_nome)
+                                sleep(1)
+                                break
+                            except ValueError as e:
+                                print(f"[red]ERRO:[/]  {e}")
+                                sleep(1)
+                                continue
+                    elif alterar.lower().strip() == "data":
+                        while True:
+                            try:
+                                nova_data = input("Digite a nova data. ex(00/00/0000): ")  
+                                academia.alterar_data_aluno(id_aluno, nova_data)
+                                sleep(1)
+                                break
+                            except ValueError as e:
+                                print(f"[red]ERRO:[/]  {e}")
+                                sleep(1)
+                                continue
+                    else:
+                        print("Opção inválida") 
+                        sleep(1)
+                        continue
+                    break    
+                         
+
+            case 4:
+                if not academia.lista_alunos:
+                    print("Ainda não temos alunos cadastrados... Voltando ao menu")
+                    sleep(1)
+                    continue
+                id_remove_aluno = academia.validar_id(academia.localizar_aluno)
+                try:
+                    aluno = academia.localizar_aluno(id_remove_aluno)
+                
+                    while True:
+                        decisao = input(f"Tem certeza que deseja remover o(a) aluno(a) {aluno.nome}? [S/N] ")  
+                        if decisao.upper().strip() == "S":
+                            academia.remover_aluno(id_remove_aluno)
+                            sleep(1)
+                            break
+                        elif decisao.upper().strip() == "N":
+                            print("Voltando para o menu...")
+                            sleep(1)
+                            break
+                        else:
+                            print("Opção inválida")
+                            sleep(1)
+                except ValueError as e:
+                    print(f"[red]ERRO:[/]  {e}")
+                    sleep(1)
+
+            case 5:
+                academia.mostrar_alunos()
+                sleep(1)
+
+            case 6:
+                while True:
+                    try:
+                        nome_prof = input("Digite o nome do professor: ")
+                        Pessoa.validar_nome(nome_prof)
+                        break
+                    except ValueError as e:
+                        print(f"[red]ERRO:[/]  {e}")
+                while True:
+                    try:
+                        data_prof = input("Digite a data de nscimento. ex(00/00/0000): ")
+                        academia.cadastrar_professor(nome_prof, data_prof)
+                        print("Professor cadastrado com [green]SUCESSO[/].")
+                        sleep(1)
+                        break
+                    except ValueError as e:
+                        print(f"[red]ERRO:[/]  {e}")
+
+            case 7:
+                prof_busca = input("Digite o nome do professor: ")
+                academia.buscar_professor(prof_busca)
+                sleep(1)
+
+            case 8:
+                if not academia.lista_professores:
+                    print("Ainda não temos professores cadastrados... Voltando ao menu")
+                    sleep(1)
+                    continue
+                id_prof = academia.validar_id(academia.localizar_professor)
+
+                while True:
+                    altera_prof = input("Alterar nome ou data? [digite apenas um]: ")
+                    if altera_prof.strip().lower() == "nome":
+                        while True:
+                            try:
+                                novo_nome_prof = input("Digite o novo nome: ")
+                                academia.alterar_nome_professor(id_prof, novo_nome_prof)
+                                sleep(1)
+                                break
+                            except ValueError as e:
+                                print(f"[red]ERRO:[/]  {e}")
+                                sleep(1)
+                                continue
+                    elif altera_prof.strip().lower() == "data":
+                        while True:
+                            try:
+                                nova_data_prof = input("Digite a nova data. ex(00/00/0000): ")
+                                academia.alterar_data_professor(id_prof, nova_data_prof)
+                                sleep(1)
+                                break
+                            except ValueError as e:
+                                print(f"[red]ERRO:[/]  {e}")
+                                sleep(1)
+                                continue
+                    else:
+                        print("Opção inválida")
+                        sleep(1)
+                        continue
+                    break
+
+            case 9:
+                if not academia.lista_professores:
+                    print("Ainda não temos professores cadastrados... Voltando ao menu")
+                    sleep(1)
+                    continue
+                id_remove_prof = academia.validar_id(academia.localizar_professor)
+                try:
+                    prof = academia.localizar_professor(id_remove_prof)
+                    while True:
+                        decisao_prof = input(f"Tem certeza que deseja remover o(a) professor(a) {prof.nome}? [S/N] ")
+                        if decisao_prof.upper().strip() == "S":
+                            academia.remover_professor(id_remove_prof)
+                            sleep(1)
+                            break
+                        elif decisao_prof.upper().strip() == "N":
+                            print("Voltando para o menu...")
+                            sleep(1)
+                            break
+                        else:
+                            print("Opção inválida")
+                            sleep(1)
+                except ValueError as e:
+                    print(f"[red]ERRO:[/]  {e}")
+                    sleep(1)
+
+            case 10:
+                academia.mostrar_professores()
+                sleep(1)
 
 if __name__ == '__main__':
     main()
--- classes.py
+++ classes.py
@@ -1,11 +1,179 @@
-from abc import ABC, abstractmethod
 from datetime import datetime, date
+import re
+from rich import print
 
 
-class Pessoa(ABC):
+class Academia:
+    def __init__(self):
+        self.lista_alunos = []
+        self.lista_professores =[]
+        self.proximo_aluno_id = 1
+        self.proximo_professor_id = 1
+        
+    def cadastra_aluno(self, nome, data_nascimento):
+        id = self.proximo_aluno_id
+        aluno = Aluno(nome, data_nascimento, id)
+        self.add_aluno(aluno)
+        self.proximo_aluno_id += 1
+        return aluno
+
+
+    def add_aluno(self,aluno):
+        self.lista_alunos.append(aluno)
+
+
+    def mostrar_alunos(self): 
+        print("==== ALUNOS ====")  
+        for aluno in self.lista_alunos:
+            print(f"[yellow]ID {aluno.id}[/] | {aluno.nome} | {aluno.idade} anos")
+
+
+
+
+    def buscar_aluno(self, nome):
+        encontrou = False
+        print('=== Busca de alunos ===')
+        for aluno in self.lista_alunos:
+            if nome.lower() in aluno.nome.lower():
+                print(f'- {aluno.nome} = {aluno.idade} anos.')
+                encontrou = True
+        if not encontrou:
+            print (f'O(A) aluno(a) {nome} não foi encontrado(a).')
+
+
+    def localizar_aluno(self, id_aluno):
+        for aluno in self.lista_alunos:
+            if id_aluno == aluno.id:
+              return aluno
+        raise ValueError("O id não existe")
+
+    def alterar_nome_aluno(self, id_aluno, novo_nome):
+        aluno = self.localizar_aluno(id_aluno)
+        aluno.nome = novo_nome
+        return aluno
+
+    def alterar_data_aluno(self, id, nova_data):
+        aluno = self.localizar_aluno(id)
+        aluno.data_nascimento = nova_data
+        return aluno
+
+    def remover_aluno(self, id):
+        aluno = self.localizar_aluno(id)
+        self.lista_alunos.remove(aluno)
+        print("Aluno(a) removido(a) com sucesso.")
+
+    def validar_id(self, localizador):
+        while True:
+            try:
+                valor = int(input("Digite a id: "))
+            except ValueError:
+                print("[red]ERRO:[/] Digite apenas números")
+                continue
+            try:
+                localizador(valor)
+                return valor
+            except ValueError as e:
+                print(f"[red]ERRO:[/]  {e}")
+
+        
+    def cadastrar_professor(self, nome, data_nascimento):
+        id = self.proximo_professor_id
+        professor = Professor(nome, data_nascimento, id)
+        self.add_professor(professor)
+        self.proximo_professor_id += 1
+        return professor
+        
+
+    def add_professor(self, professor):
+        self.lista_professores.append(professor)
+
+
+    def mostrar_professores(self):
+        print("==== PROFESSORES ====")
+        for professor in self.lista_professores:
+            print(f"[yellow]ID {professor.id}[/] | {professor.nome} | {professor.idade} anos")
+
+
+    def buscar_professor(self, nome):
+            encontrou = False
+            print("=== Busca de professor ===")
+            for professor in self.lista_professores:
+                if nome.lower() in professor.nome.lower():
+                    print(f'- {professor.nome} = {professor.idade} anos.')
+                    encontrou = True
+            if not encontrou:
+                print(f"O(A) professor(a) {nome} não foi encontrado(a).")  
+
+    
+    def localizar_professor(self, id_prof):
+        for professor in self.lista_professores:
+            if id_prof == professor.id:
+                return professor
+        raise ValueError("O id não existe")
+
+    def alterar_nome_professor(self, id_prof, novo_nome):
+        professor = self.localizar_professor(id_prof)
+        professor.nome = novo_nome
+        return professor
+
+
+    def alterar_data_professor(self, id, nova_data):
+        professor = self.localizar_professor(id)
+        professor.data_nascimento = nova_data
+        return professor
+
+    def remover_professor(self, id):
+        professor = self.localizar_professor(id)
+        self.lista_professores.remove(professor)
+        print("Professor(a) removido(a) com sucesso.")
+
+
+
+
+class Pessoa:
     def __init__(self,nome, data_nascimento):
         self.nome = nome
-        self._data_nascimento = datetime.strptime(data_nascimento, "%d/%m/%Y")
+        self.data_nascimento = data_nascimento
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @nome.setter
+    def nome(self, novo_nome):
+        Pessoa.validar_nome(novo_nome)
+        self._nome = novo_nome.strip()
+
+    @staticmethod
+    def validar_nome(nome): 
+        padrao = r"^[A-Za-zÀ-ÿ\s]+$"
+        nome = nome.strip()
+        if nome == "":
+            raise ValueError("O nome está vazio")
+        
+        if not re.match(padrao, nome):
+            raise ValueError("O nome contém carácteres inválidos")
+
+        
+
+    @property
+    def data_nascimento(self):
+        return self._data_nascimento
+
+
+
+    @data_nascimento.setter
+    def data_nascimento(self,nova_data):
+        try:
+            data_provisoria = datetime.strptime(nova_data, "%d/%m/%Y")
+        except ValueError:
+            raise ValueError("Data inválida")
+
+        if data_provisoria.date() > date.today(): 
+            raise ValueError("A data de nascimento está maior que a data atual.")
+        self._data_nascimento = data_provisoria
+    
+            
 
     @property
     def idade(self):
@@ -17,88 +185,13 @@ class Pessoa(ABC):
         return idade
                 
 
-    @abstractmethod
-    def apresentacao(self):
-        pass
-
-
 class Aluno(Pessoa):
-    def __init__(self, nome, idade):
-        super().__init__(nome, idade)
-
-    def apresentacao(self):
-        print(f'O(A) aluno(a) {self.nome} que tem {self.idade} anos foi cadastrado(a) com SUCESSO!')    
-              
+    def __init__(self, nome, data_nascimento, id):
+        super().__init__(nome, data_nascimento)
+        self.id = id              
 
 class Professor(Pessoa):
-    def __init__(self, nome, idade):
-        super().__init__(nome, idade)
-
-    def apresentacao(self):
-        print(f'O(A) professor(a) {self.nome} que tem {self.idade} anos foi cadastrado(a) com SUCESSO!')    
-
-
-class Academia():
-    def __init__(self):
-        self.lista_alunos = []
-        self.lista_professores =[]
+    def __init__(self, nome, data_nascimento, id):
+        super().__init__(nome, data_nascimento)
+        self.id = id
         
-
-    def add_aluno(self,aluno):
-        self.lista_alunos.append(aluno)
-
-
-    def mostrar_alunos(self): 
-        print("- - ALUNOS - -")  
-        for aluno in self.lista_alunos:
-            print(f"- {aluno.nome} = {aluno.idade} anos.")
-
-
-    def remover_aluno(self, nome):
-        for aluno in self.lista_alunos:
-            if aluno.nome == nome:
-                self.lista_alunos.remove(aluno)
-                print(f'Aluno(a) {nome} removido do sistema.')
-                return
-        print("O(A) aluno(a) não foi encontrado(a) em nosso sistema.")
-
-
-    def buscar_aluno(self, nome):
-        for aluno in self.lista_alunos:
-            if aluno.nome == nome:
-                print(f'Aluno(a) encontrado(a).')
-                print(f'- {aluno.nome} = {aluno.idade} anos.')
-                return
-        print(f'O(A) aluno(a) {nome} não foi encontrado(a).')
-
-                
-
-    def add_professor(self, professor):
-        self.lista_professores.append(professor)
-
-
-    def mostrar_professores(self):
-        print("- - PROFESSORES - -")
-        for professor in self.lista_professores:
-            print(f"- {professor.nome} = {professor.idade} anos.")
-            
-
-    def remover_professor(self, nome):
-        for professor in self.lista_professores:
-            if professor.nome == nome:
-                self.lista_professores.remove(professor)
-                print(f"O(A) professor(a) {nome} não faz mais parte de nossa equipe.")
-                return
-        print("Professor(a) não encontrado(a).")
-
-
-    def buscar_professor(self, nome):
-            for professor in self.lista_professores:
-                if professor.nome == nome:
-                    print(f'professor(a) encontrado(a).')
-                    print(f'- {professor.nome} = {professor.idade} anos.')
-                    return
-            print(f'O(A) professor(a) {nome} não foi encontrado(a).')
-
-    
-
